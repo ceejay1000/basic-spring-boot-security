5682e46a891ebcfc1b37e403d0f7c84a5ee1db91
unknown <emmanuelcudjoe16@yahoo.com>
2021-12-02 18:09:52 +0000

    Initial commit for spring security

---
+++ src/main/java/com/javatechie/jwt/api/controller/WelcomeController.java
@@ -0,0 +1,52 @@
+package com.javatechie.jwt.api.controller;
+
+import com.javatechie.jwt.api.entity.AuthRequest;
+import com.javatechie.jwt.api.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to javatechie !!";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "Your dashboard";
+    }
+
+    @GetMapping("/homepage")
+    public String homepage(){
+        return "Your homepage";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "Contact Page";
+    }
+
+    @PostMapping("/authenticate")
+    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword())
+            );
+        } catch (Exception ex) {
+            throw new Exception("invalid username/password");
+        }
+        return jwtUtil.generateToken(authRequest.getUserName());
+    }
+}
